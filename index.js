6a15fc1ac5ca0e6590bae84092d60b709d9309f0
Jonathan Huang <grokut@gmail.com>
2015-05-24 20:15:46 -0700

    feat/ api endpoint to get recent tweets from SF

--- index.js
+++ index.js
@@ -2,18 +2,33 @@
 
 const app = require('express')();
 
-const twitter = require('./utils/twitter-api')
+const twitter = require('./utils/twitter-api');
 const run = require('./utils/run');
 
 app.get('/', (req, res) => {
   res.send('i am a iojs server using the twitter api');
 });
 
-app.get('/iojs', (req, res) => {
+app.get('/sf', (req, res) => {
   run(function*(){
     try {
-      let tweets = yield twitter.search({q:'iojs'})
-      res.send(tweets);
+
+      let params = {
+        geocode: '37.781157,-122.398720,4mi', // San Francisco
+        count: 50,
+        lang: 'en',
+        result_type: 'recent'  
+      };
+
+      let tweets = yield twitter.search(params)
+      let text = tweets.statuses.map((tweet)=>{
+        if (!tweet.retweeted_status) return tweet.text
+        else return tweet.retweeted_status.text
+      });
+
+      console.log(text);
+      res.send(text);
+
     } catch(e) {
       res.sendStatus(404);
     }
@@ -24,6 +39,7 @@ const server = app.listen(3000, function () {
   let host = server.address().address;
   let port = server.address().port;
   console.log('iojs server listening at http://%s:%s', host, port);
+  console.log('send a GET request to localhost:3000/sf to get the most recent tweets from SF');
 });
 
 
